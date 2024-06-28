dbf4b055f316c5b9dadefcdc2469050344b6a035
Taeho Kim <taeho@taeho.io>
2024-06-28 09:31:00 +0900

    refactor: add useColor hook

--- components/external-link.tsx
+++ components/external-link.tsx
@@ -3,17 +3,15 @@ import {
   WebBrowserPresentationStyle,
   openBrowserAsync,
 } from "expo-web-browser";
-import { useColorScheme } from "nativewind";
 import { type ComponentProps } from "react";
 import { Platform } from "react-native";
 
-import { colors } from "@/constants/colors";
+import { useColor } from "@/hooks/useColorScheme";
 
 type Props = Omit<ComponentProps<typeof Link>, "href"> & { href: string };
 
 export function ExternalLink({ href, ...rest }: Props) {
-  const { colorScheme } = useColorScheme();
-  const isDark = colorScheme === "dark";
+  const backgroundColor = useColor("background");
 
   return (
     <Link
@@ -26,9 +24,7 @@ export function ExternalLink({ href, ...rest }: Props) {
           event.preventDefault();
           // Open the link in an in-app browser.
           await openBrowserAsync(href, {
-            toolbarColor: isDark
-              ? colors.dark.background
-              : colors.light.background,
+            toolbarColor: backgroundColor,
             presentationStyle: WebBrowserPresentationStyle.FULL_SCREEN,
           });
         }
--- hooks/useColorScheme.ts
+++ hooks/useColorScheme.ts
@@ -1,1 +1,13 @@
-export { useColorScheme } from "react-native";
+import { useColorScheme } from "nativewind";
+
+import { colors } from "@/constants/colors";
+
+export { useColorScheme } from "nativewind";
+
+type ColorType = keyof typeof colors.light & keyof typeof colors.dark;
+
+export const useColor = (colorName: ColorType) => {
+  const { colorScheme } = useColorScheme();
+  const color = colors[colorScheme ?? "light"][colorName];
+  return color;
+};
