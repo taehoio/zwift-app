a2490f67b4317fa93857a80d3115212c8b9de60d
Taeho Kim <taeho@taeho.io>
2024-06-28 09:54:35 +0900

    refactor: move global.css and colors.ts into styles directory (#22)

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,4 @@ const { withNativeWind } = require("nativewind/metro");
 /* eslint-disable no-undef */
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./global.css" });
+module.exports = withNativeWind(config, { input: "./styles/global.css" });
