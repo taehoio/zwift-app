a4dda33fa773175ebba32e3b90d4640fef60bf03
Taeho Kim <taeho@taeho.io>
2024-07-20 23:23:37 +0900

    feat: add jest test coverage (#29)

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
-module.exports = {
+/** @type {import('jest').Config} */
+const config = {
   // preset: "@testing-library/react-native",
   preset: "jest-expo",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
@@ -6,4 +7,9 @@ module.exports = {
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
   ],
+  collectCoverageFrom: ["**/*.{ts,tsx}", "!**/*.d.ts", "!**/node_modules/**"],
+  coverageProvider: "v8",
+  coverageReporters: ["json", "lcov", "text", "clover"],
 };
+
+module.exports = config;
