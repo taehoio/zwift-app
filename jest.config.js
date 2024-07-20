8b75ec3ce8e457017b086dacd7f106a946e1d034
Taeho Kim <taeho@taeho.io>
2024-07-20 22:54:37 +0900

    fix

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,6 @@ const config = {
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
   ],
-  collectCoverage: true,
   collectCoverageFrom: ["**/*.{ts,tsx}", "!**/*.d.ts", "!**/node_modules/**"],
   coverageProvider: "v8",
   coverageReporters: ["json", "lcov", "text", "clover"],
