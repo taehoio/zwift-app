62430fa125470a456eb47755d9f105256b14162f
Taeho Kim <taeho@taeho.io>
2024-07-27 21:37:48 +0900

    fix: show test coverage when ci (#39)

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,12 @@ const config = {
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
   ],
-  collectCoverageFrom: ["**/*.{ts,tsx}", "!**/*.d.ts", "!**/node_modules/**"],
+  collectCoverageFrom: [
+    "**/*.{ts,tsx}",
+    "!**/*.d.ts",
+    "!**/*.type.ts",
+    "!**/node_modules/**",
+  ],
   coverageProvider: "v8",
   coverageReporters: ["json", "lcov", "text", "clover"],
 };
